eefee6b8956729db114ca26f5d6cfdca9f0b703c
student <Kozin_dn78@student.itstep.org>
2023-05-18 20:30:49 +0300

    1_ex

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-#ітератор
-lst = [1,2,3,4,5,6,6]
-print(iter(lst))
-
-class MyIterator:
-    def __init__(self, data):
-        self.data = data
-        self.index = 0
-    def __iter__(self):
+class OddIterator:
+    def __init__(self, n):
+        if  n <= 0 and isinstance(n, int):
+            raise ValueError('Treba cile natur chuslo')
+        self.n = n
+        self.current = 1
+    def __next__(self):
         return self
     def __next__(self):
-        if self.index >= len(self.data):
-            raise StopIteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in MyIterator(lst):
-    print(num)
+            if self.current > self.n:
+                raise StopIteration
+            else:
+                result = self.current
+                self.current += 2
+                return result
 
-def my_generator(data):
-    for item in data:
-        yield item
+try:
+    for num in OddIterator(10):
+        print(num)
+except ValueError as e:
+    print(e)
 
-for num in my_generator(lst):
-    print(num)
 
-def calc():
-    def add(a, b):
-        return a+ b
-    def sub (a,b):
-        return a - b
-    def mult (a, b):
-        return a*b
-    def div(a,b):
-        if b != 0:
-            return a /b
-        else:
-            raise ValueError ('POMULKAAA')
-    return add, sub, mult, div
-add, sub, mult, div = calc()
-print(div(3, 1))
+
+
